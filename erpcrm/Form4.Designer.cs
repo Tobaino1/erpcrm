22e7822095739c3a563d4b763a3d07be062a9b79
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-01-17 23:09:05 +0100

    modification

--- erpcrm/Form4.Designer.cs
+++ erpcrm/Form4.Designer.cs
@@ -131,6 +131,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(605, 282);
             this.dataGridView1.TabIndex = 32;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Column1
             // 
--- erpcrm/Form4.cs
+++ erpcrm/Form4.cs
@@ -158,6 +158,11 @@ namespace erpcrm
         {
 
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
     }
 
